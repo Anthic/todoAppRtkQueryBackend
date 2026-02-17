32278df08ecd106e7a3dff206608008c65c1ae58
anthic-kumar-singh <anthickumarsingh2@gmail.com>
2026-02-17 16:56:20 +0600

    user create succesfully

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,4 +1,4 @@
-﻿﻿import type { NextFunction, Request, Response } from "express";
+﻿import type { NextFunction, Request, Response } from "express";
 import { envVariable } from "../config/env.ts";
 import { deleteFromCloudinary } from "../config/config.ts";
 import type { TErrorSource } from "../interface/erro.type.ts";
